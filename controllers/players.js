c805f03dd95f21ff8f524f6280830da399972912
piquant95 <piquant95@yahoo.com>
2023-03-08 15:23:50 -0500

    created headAndNav partial and footer partial and used ejs to share them across my views

--- controllers/players.js
+++ controllers/players.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(`<h1>This is my list of players</h1>`)
-})
+    res.render('players/index.ejs')
+});
 
 module.exports = router;
